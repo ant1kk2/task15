3792c25015c284d26b9b3d32840da9c552b94560
Anton Konstantynov <batann1990@gmail.com>
2024-01-03 08:36:59 +0200

    final

---
+++ script.js
@@ -0,0 +1,40 @@
+const input = document.querySelector(".task-creator__input");
+const addBtn = document.querySelector(".task-creator__btn");
+const taskList = document.querySelector(".task__list");
+
+function addTask() {
+  const taskTitle = input.value;
+
+  if (taskTitle.trim() === "") {
+    return;
+  }
+
+  input.value = "";
+
+  const task = document.createElement("li");
+  task.className = "task";
+
+  const title = document.createElement("span");
+  title.className = "task__title";
+  title.textContent = taskTitle;
+  task.append(title);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.className = "task__del-btn";
+  deleteBtn.textContent = "delete";
+  task.append(deleteBtn);
+
+  taskList.append(task);
+  taskList.addEventListener("click", deleteTask);
+}
+
+function deleteTask({ target }) {
+  if (target.classList.contains("task__del-btn")) {
+    target.parentElement.remove();
+  }
+  if (!taskList.children.length) {
+    taskList.removeEventListener("click", deleteTask);
+  }
+}
+
+addBtn.addEventListener("click", addTask);
